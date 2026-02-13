d8f76b5aa68774bd7128f26d2c972ef9b082bf29
kirti berekar <berekarkirti11@gmail.com>
2026-02-13 22:35:25 +0530

    task done

--- src/components/dashboard/TasksTab.tsx
+++ src/components/dashboard/TasksTab.tsx
@@ -7,7 +7,7 @@ const TasksTab = () => {
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
           </svg>
           <h3 className="text-xl font-semibold text-gray-700 mb-2">Tasks</h3>
-          <p className="text-gray-500">Content will be added soon</p>
+          <p className="text-gray-500">Content will be added soon.</p>
         </div>
       </div>
     </div>
